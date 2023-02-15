86b7474b31262e6cb39b07e6558b49f01270f6e7
Jayvardhan Prajapat <103930724+jayvardhan2111@users.noreply.github.com>
2023-02-15 09:46:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) =>
   res.send(
-   "Welcome to simple nodejs  Web application, cd is based on Argo, In future we will implement automatic triger "
+   "Welcome to simple nodejs  Web application, cd is based on Argo "
   )
 );
 
