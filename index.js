4684576c428ce713717c0b31b6eab72c907d2bfc
Jayvardhan Prajapat <103930724+jayvardhan2111@users.noreply.github.com>
2023-03-04 00:20:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) =>
   res.send(
-   "Welcome to simple nodejs  Web application, cd is based on Argo and cd is on github actions "
+   "Welcome to simple nodejs  Web application, cd is based on Argo and cd is on github actions ... dEPLOYED ON kubernetes local cluster  "
   )
 );
 
